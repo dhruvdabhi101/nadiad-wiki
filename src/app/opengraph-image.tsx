8076404ff75b8e4bca8ba85ad23b74bef6e9a5a4
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-07-18 14:25:08 +0530

    updated opengraph image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,48 +1,46 @@
-import { ImageResponse } from 'next/og'
+import { ImageResponse } from "next/og";
 
 // Image metadata
-export const alt = 'nadiad.wiki'
+export const alt = "nadiad.wiki";
 export const size = {
   width: 1200,
   height: 630,
-}
+};
 
-export const contentType = 'image/png'
+export const contentType = "image/png";
 
 // Image generation
 export default async function Image() {
-
   return new ImageResponse(
     (
       // ImageResponse JSX element
       <div
         style={{
           fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
         <span
           style={{
-            fontFamily: 'monospace',
-            color: 'black',
+            fontFamily: "monospace",
+            color: "black",
             fontSize: 128,
-            fontWeight: 'bold',
+            fontWeight: "bold",
           }}
         >
-          nadiad
+          🎇 nadiad
         </span>
         <span
           style={{
-            fontFamily: 'monospace',
-            color: '#9ca3af',
+            fontFamily: "monospace",
+            color: "#9ca3af",
             fontSize: 128,
-            fontWeight: 'bold',
-
+            fontWeight: "bold",
           }}
         >
           .wiki
@@ -51,6 +49,6 @@ export default async function Image() {
     ),
     {
       ...size,
-    }
-  )
+    },
+  );
 }
